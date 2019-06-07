ae739c68b2c53f9a487228b053f6e208b1d41a15
Taylor <33495284+Tails59@users.noreply.github.com>
2019-06-07 10:06:04 +0100

    serverTenthSecond hook

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,5 +1,9 @@
 include("include.lua")
 AddCSLuaFile("include.lua")
 
+function serverTenthSecond()
+	hook.Call("serverTenthSecond")
+end
+timer.Create("serverTenthSecond", 0.1, 0, serverTenthSecond)
 
 
